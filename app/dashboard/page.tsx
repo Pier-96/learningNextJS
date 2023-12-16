0c7d4d483ee718a2e99fe7890d9a42deb820a8fa
Piero Olivares Palomino <pieroolivarespalomino@MacBook-Air-de-Piero.local>
2023-12-16 12:24:20 +0100

    feat: first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link"
+export default function Page() {
+    return <main>
+        <p>Sigan viendo aqui en este link </p>
+    <Link className="text-red-600 text-9xl font-bold" href="/dashboard/agusmierdon">CLICKAME CABRÓN</Link>
+    </main>
+}
