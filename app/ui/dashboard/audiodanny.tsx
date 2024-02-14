e9370680b49802152f521872c3852617ec66b371
Piero Olivares Palomino <pieroolivarespalomino@Air-de-Piero.home>
2024-02-14 15:33:11 +0100

    feat: add dannylove path

--- app/ui/dashboard/audiodanny.tsx
+++ app/ui/dashboard/audiodanny.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useRef } from 'react';
 
 // Importa el archivo de audio si es necesario
-import AudioFile from '../../../public/danny.mp3';
 
 const DannyAudio = () => {
-  const audioRef = useRef(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const reproducirAudio = () => {
     if (audioRef.current) {
@@ -16,7 +15,7 @@ const DannyAudio = () => {
     <div>
       <button className={`bg-pink-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`} onClick={reproducirAudio}>Click para sonido🔊</button>
       <audio ref={audioRef} autoPlay>
-        <source src={AudioFile} type="audio/mpeg" />
+        <source src="/danny.mp3" type="audio/mpeg" />
         Tu navegador no soporta la reproducción de audio.
       </audio>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 // next.config.js
 module.exports = {
     webpack: (config, { isServer }) => {
